1df7ad0f9954d324bb6b57930bbdd86f392bd900
Charles Polart <charles.polart@hotmail.fr>
2021-06-28 23:40:15 +0200

    compute profit, compute total

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
 import Koa from "koa";
 import Router from "koa-router";
-import { Pool } from "pg";
+import { Pool, QueryResultRow } from "pg";
 
 const app = new Koa();
 const router = new Router();
 const pool = new Pool({
-    database: process.env.DATABASE_RESULT_HISTORY_NAME,
+    database: process.env.DATABASE_RESULT_NAME,
     user: process.env.DATABASE_USER,
     password: process.env.DATABASE_PASSWORD,
 });
@@ -16,13 +16,74 @@ router.get("/api/status", async (ctx) => {
     ctx.body = "ok";
 });
 
-router.get("/api/pairs", async (ctx) => {
+router.get("/api/getPair", async (ctx) => {
+    ctx.body = await queryGetPair();
+});
+
+router.get("/api/total", async (ctx) => {
+    ctx.body = await computeTotalProfit();
+});
+
+router.get("/api/total/:pair", async (ctx) => {
+    ctx.body = await computeProfit(ctx.params.pair);
+});
+
+async function queryGetPair(): Promise<QueryResultRow> {
     const { rows } = await pool.query(
         "SELECT DISTINCT pair FROM transactions ORDER BY pair",
         []
     );
-    ctx.body = rows;
-});
+    return (rows);
+}
+
+async function queryGetTransactions(pair: string): Promise<QueryResultRow> {
+    const { rows } = await pool.query(
+        "SELECT close_price, type FROM transactions WHERE pair='" + pair + "' ORDER BY close_time",
+        []
+    );
+    return (rows);
+}
+
+async function computeTotalProfit() {
+    const result: { [key: string]: unknown } = {};
+    let total = 0.0;
+    const pairs = await queryGetPair();
+
+    for (let i = 0; i < pairs.length; ++i) {
+        const profit = await computeProfit(pairs[i].pair);
+        total += profit["total"] as number;
+    }
+    result["total"] = total;
+    return (result);
+}
+
+async function computeProfit(pair: string) {
+    const result: { [key: string]: unknown } = {};
+    let total = 0.0;
+    const rows: QueryResultRow = await queryGetTransactions(pair);
+    let buy = 0.0;
+    let i = 0;
+
+    if (rows.length === 0) {
+        result["error"] = "pair does not exist";
+        return (result);
+    }
+    for (; rows.length > 0 && rows[i].type != "buy"; ++i);
+    for (; i < rows.length; ++i) {
+        if (rows[i].type === "buy") {
+            buy = rows[i].close_price;
+        }
+        else if (rows[i].type === "sell" && buy !== 0.0) {
+            const sell = rows[i].close_price;
+            const profit = (sell - buy) / buy * 100.0;
+            total += profit;
+            buy = 0.0;
+        }
+    }
+    result["pair"] = pair;
+    result["total"] = total;
+    return (result);
+}
 
 app.use(router.routes());
 
